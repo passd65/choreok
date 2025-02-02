9221f20ef67e9b00d1fcb700edb28b4fe4803faa
passd65 <hellwordesk@proton.me>
2025-02-01 19:53:17 -0800

    Update index.js

--- files/index.js
+++ files/index.js
@@ -6,8 +6,8 @@ const UUID = process.env.UUID || 'a2a687bf-c8c2-4438-813e-b2633250c065'; //若�
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'nz.f4i.cn';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '5555';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
 const NEZHA_KEY = process.env.NEZHA_KEY || '5ddVS93Eq0Uc9he880';
-const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'chot13.tiimngvfsd.cf';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
-const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYjg3ZTc3MmM3YjY3OGNhYzE4OTI3ZjQ3NzEyODBhODciLCJ0IjoiZDY2MDg4NzgtNjllMi00NGQxLWE3YmYtZTkxYzhhM2FmYzZjIiwicyI6Ik1UZzNZalE0WTJZdE1EZ3laUzAwTURZNExUaGxNR1V0WlROaE1qazNNMkprTmpRdyJ9';
+const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'fareco.12ort0dkl.us.kg';     // 建议使用token，argo端口8080，cf后台设置需对应,使用json需上传json和yml文件至files目录
+const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiYjg1NjNlNzMyM2Q2NTM4MGM2M2IwNzY0OTZlMDhmM2IiLCJ0IjoiODc2MDkzNmUtNDY2Zi00OWI5LWJjZjAtMGRiZDM2YWNlODgzIiwicyI6Ik1UazRNV1JrTTJNdFkyUmxZUzAwT0RVMUxXRm1OR0l0TnpnM1kyVm1NakEwWm1VdyJ9';
 const CFIP = process.env.CFIP || 'na.ma';
 const NAME = process.env.NAME || 'Choreo';
 
